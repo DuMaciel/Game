cd7442a0d99d7a75802354bccd20c009634abd2c
DuMaciel <edumaciel1221@gmail.com>
2022-11-16 21:23:55 -0300

    feat: Create fruits

--- src/server.js
+++ src/server.js
@@ -3,13 +3,31 @@ const http = require('http');
 const express = require('express');
 const Server = require('socket.io');
 const Player = require('./models/player');
+const Fruit = require('./models/fruit');
+const { randomWithWeight } = require('./utils');
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const TICK_TIME = (1 / 30) * 1000;
+const FRUIT_TIME = 5 * 1000;
+
+const FRUIT_TABLE = [
+  { score: 1, weight: 32 },
+  { score: 2, weight: 16 },
+  { score: 3, weight: 4 },
+  { score: 5, weight: 2 },
+  { score: 8, weight: 1 },
+];
+
+
+
 app.use('/', express.static('public'));
 
 const players = [];
+const fruits = [];
+
 io.on('connection', (socket) => {
   console.log('a user connected');
   const player = new Player(socket.id);
@@ -33,3 +51,13 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
   console.log('listening on http://localhost:3000');
 });
+
+setInterval(() => {
+  const { score } = FRUIT_TABLE[randomWithWeight(FRUIT_TABLE)]
+  const fruit = new Fruit(score);
+  fruits.push(fruit);
+}, FRUIT_TIME);
+
+setInterval(() => {
+  io.emit('tick', { players, fruits });
+}, TICK_TIME);
