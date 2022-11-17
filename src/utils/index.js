2c5efea2c2dadb2f1325a4ff7bfab038e7f8e81b
DuMaciel <edumaciel1221@gmail.com>
2022-11-16 22:31:53 -0300

    feat: Random with weight

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,29 @@
 function randomPosition(max) {
   return Math.floor(Math.random() * max + 0.999999);
 }
+
+function randomWithWeight(table) {
+  const total = table.reduce((acc, t) => t.weight + acc, 0);
+  const random = Math.floor(Math.random() * total + 0.999999);
+
+  const { index } = table.reduce((prev, curr, currIndex) => {
+    if (prev.index) return prev;
+
+    const mod = prev.mod - curr.weight;
+    if (mod <= 0) {
+      return {
+        index: currIndex,
+        mod,
+      };
+    }
+
+    return {
+      ...prev,
+      mod,
+    };
+  }, { index: undefined, mod: random });
+
+  return index;
 }
 
-module.exports = { randomPosition };
+module.exports = { randomPosition, randomWithWeight };
