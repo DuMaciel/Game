edeedfed8482d47dcf2482c43b88d7f0d4e90c29
DuMaciel <edumaciel1221@gmail.com>
2022-11-16 22:31:07 -0300

    refector: Random position with max range

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
-function randomPosition() {
-  return Math.floor(Math.random() * 11);
+function randomPosition(max) {
+  return Math.floor(Math.random() * max + 0.999999);
+}
 }
 
 module.exports = { randomPosition };
